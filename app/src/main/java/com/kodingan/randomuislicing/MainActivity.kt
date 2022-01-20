9806f23f9ee52186bbe29acdc384c7948da813aa
ahmadkabi <kabbi@wlb.co.id>
2022-01-20 08:44:31 +0700

    1

--- app/src/main/java/com/kodingan/randomuislicing/MainActivity.kt
+++ app/src/main/java/com/kodingan/randomuislicing/MainActivity.kt
@@ -2,6 +2,8 @@ package com.kodingan.randomuislicing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
 import androidx.appcompat.widget.AppCompatImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -11,16 +13,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        buildItemStoryMine()
+
+        /*1*/ buildItemStoryMine()
 
     }
 
-    private fun buildItemStoryMine(){
-        val image = findViewById<AppCompatImageView>(R.id.image)
+    private fun inflateLayout(layoutRes: Int): View {
+        val root = findViewById<FrameLayout>(R.id.container)
+
+        return layoutInflater.inflate(
+            layoutRes,
+            root,
+            false
+        )
+
+    }
+
+    private fun buildItemStoryMine() {
+        val inflatedLayout = inflateLayout(R.layout.item_story_mine)
+
+        val image = inflatedLayout.findViewById<AppCompatImageView>(R.id.image)
         Glide.with(this)
             .load(R.drawable.cat)
             .apply(RequestOptions.circleCropTransform())
             .into(image)
+
     }
 
 }
