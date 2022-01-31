2766a0185023832c96b9da0c49b517d4101c2caa
ahmadkabi <kabbi@wlb.co.id>
2022-01-31 07:56:10 +0700

    PRESENTATION PREPARATION

--- app/src/main/java/com/kodingan/randomuislicing/MainActivity.kt
+++ app/src/main/java/com/kodingan/randomuislicing/MainActivity.kt
@@ -3,8 +3,6 @@ package com.kodingan.randomuislicing
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.kodingan.randomuislicing.databinding.ActivityMainBinding
-import com.kodingan.randomuislicing.databinding.ScreenIgStoryBinding
-import com.kodingan.view.IGStoryView.Companion.stateUnread
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,21 +14,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /*1*/ buildItemStory()
-
-    }
-
-    private fun buildItemStory() {
-        val binding = ScreenIgStoryBinding.inflate(
-            layoutInflater,
-            binding.container,
-            true
-        )
-
-        binding.igStoryView.setOnClickListener {
-            binding.igStoryView.setState(stateUnread)
-        }
-        
     }
 
 }
