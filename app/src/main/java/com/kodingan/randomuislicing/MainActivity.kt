12d5962de8bbb26066ff94493636059cd5db9bf8
ahmadkabi <kabbi@wlb.co.id>
2022-01-31 07:41:01 +0700

    preparation

--- app/src/main/java/com/kodingan/randomuislicing/MainActivity.kt
+++ app/src/main/java/com/kodingan/randomuislicing/MainActivity.kt
@@ -1,43 +1,36 @@
 package com.kodingan.randomuislicing
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.FrameLayout
-import androidx.appcompat.widget.AppCompatImageView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
+import androidx.appcompat.app.AppCompatActivity
+import com.kodingan.randomuislicing.databinding.ActivityMainBinding
+import com.kodingan.randomuislicing.databinding.ScreenIgStoryBinding
+import com.kodingan.view.IGStoryView.Companion.stateUnread
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-//        /*1*/ buildItemStoryMine()
+        /*1*/ buildItemStory()
 
     }
 
-    private fun inflateLayout(layoutRes: Int): View {
-        val root = findViewById<FrameLayout>(R.id.container)
-
-        return layoutInflater.inflate(
-            layoutRes,
-            root,
+    private fun buildItemStory() {
+        val binding = ScreenIgStoryBinding.inflate(
+            layoutInflater,
+            binding.container,
             true
         )
 
-    }
-
-    private fun buildItemStoryMine() {
-        val inflatedLayout = inflateLayout(R.layout.item_story_mine)
-
-        val image = inflatedLayout.findViewById<AppCompatImageView>(R.id.image)
-        Glide.with(this)
-            .load(R.drawable.cat)
-            .apply(RequestOptions.circleCropTransform())
-            .into(image)
-
+        binding.igStoryView.setOnClickListener {
+            binding.igStoryView.setState(stateUnread)
+        }
+        
     }
 
 }
--- app/src/main/java/com/kodingan/view/IGStoryView.kt
+++ app/src/main/java/com/kodingan/view/IGStoryView.kt
@@ -9,34 +9,37 @@ import androidx.constraintlayout.widget.ConstraintSet
 import androidx.constraintlayout.widget.ConstraintSet.*
 import androidx.core.content.ContextCompat
 import androidx.core.view.setPadding
+import com.bumptech.glide.Glide
 import com.kodingan.randomuislicing.R
 
 
-class IGStoryView(context: Context, val attrs: AttributeSet) : ConstraintLayout(context, attrs) {
+class IGStoryView(context: Context, private val attrs: AttributeSet) : ConstraintLayout(context, attrs) {
 
-    private var state: Int = 1
+    private var state: Int = stateNone
     private val defaultWidth = resources.getDimension(R.dimen.h_story).toInt()
     private val defaultHeight = resources.getDimension(R.dimen.h_story).toInt()
     private var layoutWidth = WRAP_CONTENT
     private var layoutHeight = WRAP_CONTENT
 
     init {
+
         setPadding(resources.getDimensionPixelSize(R.dimen.dp_5))
 
         getAttrs()
         buildImage()
 
         when (state) {
-            0 -> {
+            stateNone -> {
                 insertAddIcon()
             }
-            1 -> {
+            stateUnread -> {
                 background = ContextCompat.getDrawable(context, R.drawable.bg_story_ring_on)
             }
-            2 -> {
+            stateRead -> {
                 background = ContextCompat.getDrawable(context, R.drawable.bg_story_ring_off)
             }
         }
+
     }
 
     private fun getAttrs() {
@@ -46,31 +49,32 @@ class IGStoryView(context: Context, val attrs: AttributeSet) : ConstraintLayout(
             0,
             0
         ).apply {
-            state = getInteger(R.styleable.IGStoryView_state, 0)
-            layoutWidth  = getLayoutDimension(R.styleable.IGStoryView_android_layout_height, 0)
-            layoutHeight  = getLayoutDimension(R.styleable.IGStoryView_android_layout_width, 0)
+            state = getInteger(R.styleable.IGStoryView_state, stateNone)
+            layoutWidth = getLayoutDimension(R.styleable.IGStoryView_android_layout_height, 0)
+            layoutHeight = getLayoutDimension(R.styleable.IGStoryView_android_layout_width, 0)
+
             recycle()
         }
+
     }
 
     private fun buildImage() {
         val imageView = AppCompatImageView(context)
         val params = LayoutParams(
-            if(layoutWidth == WRAP_CONTENT) defaultWidth else MATCH_PARENT,
-            if(layoutHeight == WRAP_CONTENT) defaultHeight else MATCH_PARENT
+            if (layoutWidth == WRAP_CONTENT) defaultWidth else MATCH_PARENT,
+            if (layoutHeight == WRAP_CONTENT) defaultHeight else MATCH_PARENT
         )
         imageView.layoutParams = params
 
 //        imageView.setImageResource(R.drawable.cat)
 //        imageView.scaleType = ImageView.ScaleType.CENTER_CROP
 
-//        Glide.with(context)
-//            .load(R.drawable.cat)
-//            .circleCrop()
-//            .into(imageView)
+        Glide.with(context)
+            .load(R.drawable.cat)
+            .circleCrop()
+            .into(imageView)
 
         imageView.id = R.id.ig_story_image
-
 
         addView(imageView)
 
@@ -93,6 +97,34 @@ class IGStoryView(context: Context, val attrs: AttributeSet) : ConstraintLayout(
         set.connect(imageView.id, RIGHT, PARENT_ID, RIGHT)
         set.connect(imageView.id, BOTTOM, PARENT_ID, BOTTOM)
         set.applyTo(this)
+
+    }
+
+    fun setState(state: Int) {
+        if(this.state != state){
+            if (this.state == stateNone) removeViewAt(1)
+
+            this.state = state
+
+            when (state) {
+                stateNone -> {
+                    insertAddIcon()
+                    background = ContextCompat.getDrawable(context, R.drawable.bg_none)
+                }
+                stateUnread -> {
+                    background = ContextCompat.getDrawable(context, R.drawable.bg_story_ring_on)
+                }
+                stateRead -> {
+                    background = ContextCompat.getDrawable(context, R.drawable.bg_story_ring_off)
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val stateNone = 0
+        const val stateUnread = 1
+        const val stateRead = 2
 
     }
 
